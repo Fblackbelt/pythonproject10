b36b4862eb7509e68868aeb7c9d023db49742c82
Fblackbelt <fwild1@unh.newhaven.edu>
2023-10-18 20:43:28 -0400

    Final

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import requests
 import json
 
 def PlayerCount(steamID):
-    API = "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1"
+    API = "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/"
     data = {
             "appid" : steamID
-            }
+    }
     response = requests.get(API, data)
-    print(response.status_code)
     if response.status_code != 200:
-        print("Steam DB API is down, please retry later")
-        exit(0)
+        print("Token is invalid or the Steam DB API is down, please retry")
     else:
         my_json = json.loads(response.text)
         print(f"Current player count for ID {steamID} = {my_json['response']['player_count']}")
